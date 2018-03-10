383f124569f50f0ec6f80c9e03ab6d6fc6337db3
Rubin <dalamar84@mail.ru>
2018-03-10 10:03:20 -0500

    extend currency list

--- src/Currency.php
+++ src/Currency.php
@@ -6,7 +6,37 @@ use unapi\helper\types\Enum;
 
 class Currency extends Enum
 {
-    const USD = 'USD';
-    const EUR = 'EUR';
-    const RUB = 'RUB';
+    const AUD = 'AUD'; // Австралийский доллар
+    const AZN = 'AZN'; // Азербайджанский манат
+    const GBP = 'GBP'; // Фунт стерлингов Соединенного королевства
+    const AMD = 'AMD'; // Армянский драм
+    const BYN = 'BYN'; // Белорусский рубль
+    const BGN = 'BGN'; // Болгарский лев
+    const BRL = 'BRL'; // Бразильский реал
+    const HUF = 'HUF'; // Венгерский форинт
+    const HKD = 'HKD'; // Гонконгский доллар
+    const DKK = 'DKK'; // Датская крона
+    const USD = 'USD'; // Доллар США
+    const RUB = 'RUB'; // Российский рубль
+    const EUR = 'EUR'; // Евро
+    const INR = 'INR'; // Индийская рупия
+    const KZT = 'KZT'; // Казахстанский тенге
+    const CAD = 'CAD'; // Канадский доллар
+    const KGS = 'KGS'; // Киргизский сом
+    const CNY = 'CNY'; // Китайский юань
+    const MDL = 'MDL'; // Молдавский лей
+    const NOK = 'NOK'; // Норвежская крона
+    const PLN = 'PLN'; // Польский злотый
+    const RON = 'RON'; // Румынский лей
+    const SGD = 'SGD'; // Сингапурский доллар
+    const TJS = 'TJS'; // Таджикский сомони
+    const TRY = 'TRY'; // Турецкая лира
+    const TMT = 'TMT'; // Новый туркменский манат
+    const UZS = 'UZS'; // Узбекский сум
+    const UAH = 'UAH'; // Украинская гривна
+    const CZK = 'CZK'; // Чешская крона
+    const CHF = 'CHF'; // Швейцарский франк
+    const ZAR = 'ZAR'; // Южноафриканский рэнд
+    const KRW = 'KRW'; // Вон Республики Корея
+    const JPY = 'JPY'; // Японская иена
 }
